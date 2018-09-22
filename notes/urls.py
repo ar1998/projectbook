ab281a57d57a0bf6a7c63cadf397cb6983840b00
ramanaditya <adityaraman96@gmail.com>
2018-09-22 15:27:37 +0530

    .

--- notes/urls.py
+++ notes/urls.py
@@ -4,13 +4,8 @@ from . import views
 app_name = 'notes'
 
 urlpatterns = [
-<<<<<<< HEAD
-    #path('user_login/',views.user_login,name='user_login'),
-    path('user_login/',views.UserLoginView.as_view(),name='user_login'),
-=======
     path('user_login/',views.user_login,name='user_login'),
     #path('user_login/',views.UserLoginView.as_view(),name='user_login'),
->>>>>>> 2ad5d675d0f3c37349432beb4754744a4f168e24
     path('user_logout/',views.LogoutView.as_view(),name='logout'),
     path('register/',views.register,name='register'),
     path('notes/',views.file_python,name = 'file_python'),
